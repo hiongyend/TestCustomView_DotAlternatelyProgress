a41ea597ad44c505c53618587cd83abe2b9754d2
hiongyend <171970918@qq.com>
2017-06-16 21:51:08 +0800

    first commit

---
+++ app/src/main/java/com/kincai/testcustomview_dotalternatelyprogress/DotAlternatelyView.java
@@ -0,0 +1,31 @@
+package com.kincai.testcustomview_dotalternatelyprogress;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Copyright (C) 2015 The KINCAI Open Source Project
+ * .
+ * Create By KINCAI
+ * .
+ * Time 2017-06-16 21:44
+ * .
+ * Desc TODO
+ */
+
+public class DotAlternatelyView extends View {
+
+    public DotAlternatelyView(Context context) {
+        this(context, null);
+    }
+
+    public DotAlternatelyView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DotAlternatelyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/kincai/testcustomview_dotalternatelyprogress/MainActivity.java
@@ -0,0 +1,23 @@
+package com.kincai.testcustomview_dotalternatelyprogress;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright (C) 2015 The KINCAI Open Source Project
+ * .
+ * Create By KINCAI
+ * .
+ * Time 2017-06-16 21:41
+ * .
+ * Desc TODO
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
