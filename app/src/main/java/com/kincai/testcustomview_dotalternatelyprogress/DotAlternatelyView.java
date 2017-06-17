9c04f3835143b9560aa5f7415d4edc5d8b109346
hiongyend <171970918@qq.com>
2017-06-17 19:14:19 +0800

    change code

--- app/src/main/java/com/kincai/testcustomview_dotalternatelyprogress/DotAlternatelyView.java
+++ app/src/main/java/com/kincai/testcustomview_dotalternatelyprogress/DotAlternatelyView.java
@@ -22,23 +22,56 @@ import android.view.View;
 
 public class DotAlternatelyView extends View {
     private final String TAG = this.getClass().getSimpleName();
-    private Paint mDarkPaint = new Paint();
-    private int mDarkColor;
-    private int mLightColor;
+    private Paint mPaint = new Paint();
+    /**
+     * 可视为左边圆点颜色值
+     */
+    private int mLeftColor;
+    /**
+     * 可视为右边圆点颜色值
+     */
+    private int mRightColor;
+    /**
+     * 圆点半径
+     */
     private int mDotRadius;
+    /**
+     * 圆点间距
+     */
     private int mDotSpacing;
+    /**
+     * 圆点位移量
+     */
     private float mMoveDistance;
+    /**
+     * 圆点移动率
+     */
     private float mMoveRate;
-    /** 以刚开始左边圆点为准 向右移*/
+    /**
+     * 以刚开始左边圆点为准 向右移
+     */
     private final int DOT_STATUS_RIGHT = 0X101;
-    /** 以刚开始左边圆点为准 圆点移动方向-向左移*/
+    /**
+     * 以刚开始左边圆点为准 圆点移动方向-向左移
+     */
     private final int DOT_STATUS_LEFT = 0X102;
-    /** 以刚开始左边圆点为准，圆点移动方向*/
+    /**
+     * 以刚开始左边圆点为准，圆点移动方向
+     */
     private int mDotChangeStatus = DOT_STATUS_RIGHT;
-
+    /**
+     * 圆点透明度变化最大(也就是透明度在255-mAlphaChangeTotal到255之间)
+     */
     private int mAlphaChangeTotal = 100;
+    /**
+     * 透明度变化率
+     */
     private float mAlphaChangeRate;
+    /**
+     * 透明度改变量
+     */
     private float mAlphaChange;
+
     public DotAlternatelyView(Context context) {
         this(context, null);
     }
@@ -56,12 +89,13 @@ public class DotAlternatelyView extends View {
     }
 
     private void initAttributes(TypedArray Attributes) {
-        mDarkColor = Attributes.getColor(R.styleable.DotAlternatelyView_dot_dark_color, ContextCompat.getColor(getContext(),R.color.colorPrimary));
-        mLightColor = Attributes.getColor(R.styleable.DotAlternatelyView_dot_light_color, ContextCompat.getColor(getContext(),R.color.colorAccent));
-        mDotRadius = Attributes.getDimensionPixelSize(R.styleable.DotAlternatelyView_dot_radius,DensityUtils.dp2px(getContext(),3));
-        mDotSpacing = Attributes.getDimensionPixelSize(R.styleable.DotAlternatelyView_dot_spacing,DensityUtils.dp2px(getContext(),6));
-        mMoveRate = Attributes.getFloat(R.styleable.DotAlternatelyView_dot_move_rate,1.2f);
+        mLeftColor = Attributes.getColor(R.styleable.DotAlternatelyView_dot_dark_color, ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        mRightColor = Attributes.getColor(R.styleable.DotAlternatelyView_dot_light_color, ContextCompat.getColor(getContext(), R.color.colorAccent));
+        mDotRadius = Attributes.getDimensionPixelSize(R.styleable.DotAlternatelyView_dot_radius, DensityUtils.dp2px(getContext(), 3));
+        mDotSpacing = Attributes.getDimensionPixelSize(R.styleable.DotAlternatelyView_dot_spacing, DensityUtils.dp2px(getContext(), 6));
+        mMoveRate = Attributes.getFloat(R.styleable.DotAlternatelyView_dot_move_rate, 1.2f);
     }
+
     /**
      * 初始化
      */
@@ -69,10 +103,10 @@ public class DotAlternatelyView extends View {
         //移动总距离/移动率 = alpha总变化/x
         //x = 移动率 * alpha总变化 / 移动总距离
         mAlphaChangeRate = mMoveRate * mAlphaChangeTotal / (mDotRadius * 2 + mDotSpacing);
-        mDarkPaint.setColor(mDarkColor);
-        mDarkPaint.setAntiAlias(true);
-        mDarkPaint.setStyle(Paint.Style.FILL);
-        Log.e(TAG, " aaaa "+mAlphaChangeRate);
+        mPaint.setColor(mLeftColor);
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+        Log.e(TAG, " aaaa " + mAlphaChangeRate);
     }
 
     @Override
@@ -85,64 +119,68 @@ public class DotAlternatelyView extends View {
         int width;
         int height;
 
-        if(widthMode == MeasureSpec.EXACTLY) {
+        if (widthMode == MeasureSpec.EXACTLY) {
             width = widthSize;
-            Log.e(TAG, "onMeasure MeasureSpec.EXACTLY widthSize="+widthSize);
+            Log.e(TAG, "onMeasure MeasureSpec.EXACTLY widthSize=" + widthSize);
         } else {
             //指定最小宽度所有圆点加上间距的宽度, 以最小半径加上间距算总和再加上最左边和最右边变大后的距离
             width = (mDotRadius * 2) * 2 + mDotSpacing;
-            Log.e(TAG, "onMeasure no MeasureSpec.EXACTLY widthSize="+widthSize+" width="+width);
-            if(widthMode == MeasureSpec.AT_MOST) {
+            Log.e(TAG, "onMeasure no MeasureSpec.EXACTLY widthSize=" + widthSize + " width=" + width);
+            if (widthMode == MeasureSpec.AT_MOST) {
                 width = Math.min(width, widthSize);
-                Log.e(TAG, "onMeasure MeasureSpec.AT_MOST width="+width);
+                Log.e(TAG, "onMeasure MeasureSpec.AT_MOST width=" + width);
             }
 
         }
 
-        if(heightMode == MeasureSpec.EXACTLY) {
+        if (heightMode == MeasureSpec.EXACTLY) {
             height = heightSize;
-            Log.e(TAG, "onMeasure MeasureSpec.EXACTLY heightSize="+heightSize);
+            Log.e(TAG, "onMeasure MeasureSpec.EXACTLY heightSize=" + heightSize);
         } else {
             height = mDotRadius * 2;
-            Log.e(TAG, "onMeasure no MeasureSpec.EXACTLY heightSize="+heightSize+" height="+height);
-            if(heightMode == MeasureSpec.AT_MOST) {
+            Log.e(TAG, "onMeasure no MeasureSpec.EXACTLY heightSize=" + heightSize + " height=" + height);
+            if (heightMode == MeasureSpec.AT_MOST) {
                 height = Math.min(height, heightSize);
-                Log.e(TAG, "onMeasure MeasureSpec.AT_MOST height="+height);
+                Log.e(TAG, "onMeasure MeasureSpec.AT_MOST height=" + height);
             }
 
         }
-        setMeasuredDimension(width,height);
+        setMeasuredDimension(width, height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        //左边圆点起点x轴
         int startPointX = getWidth() / 2 - (2 * mDotRadius * 2 + mDotSpacing) / 2 + mDotRadius;
+        //左边圆点起点y轴
         int startPointY = getHeight() / 2;
-        if(mDotChangeStatus == DOT_STATUS_RIGHT) {
+        //向右移 位移要增加对应透明度变化量也需要增加 反之都需要减小
+        if (mDotChangeStatus == DOT_STATUS_RIGHT) {
             mMoveDistance += mMoveRate;
             mAlphaChange += mAlphaChangeRate;
         } else {
             mAlphaChange -= mAlphaChangeRate;
             mMoveDistance -= mMoveRate;
         }
-        Log.e(TAG,"mAlphaChange "+mAlphaChange);
-        if(mMoveDistance >= mDotRadius * 2 + mDotSpacing && mDotChangeStatus == DOT_STATUS_RIGHT) {
+        Log.e(TAG, "mAlphaChange " + mAlphaChange);
+        //当移动到最右 那么需要改变方向 反过来
+        if (mMoveDistance >= mDotRadius * 2 + mDotSpacing && mDotChangeStatus == DOT_STATUS_RIGHT) {
             mDotChangeStatus = DOT_STATUS_LEFT;
             mMoveDistance = mDotRadius * 2 + mDotSpacing;
             mAlphaChange = mAlphaChangeTotal;
-        } else if(mMoveDistance <= 0 && mDotChangeStatus == DOT_STATUS_LEFT) {
+        } else if (mMoveDistance <= 0 && mDotChangeStatus == DOT_STATUS_LEFT) { //当移动到最座 那么需要改变方向 反过来
             mDotChangeStatus = DOT_STATUS_RIGHT;
-            mMoveDistance = 0;
-            mAlphaChange = 0;
+            mMoveDistance = 0f;
+            mAlphaChange = 0f;
         }
 
-        mDarkPaint.setColor(mDarkColor);
-        mDarkPaint.setAlpha((int) (255 - mAlphaChange));
-        //画不同颜色的圆点 其实可以用一个画笔就可以搞定 这里是为了方便
-        canvas.drawCircle(startPointX + mMoveDistance, startPointY, mDotRadius, mDarkPaint);
-        mDarkPaint.setColor(mLightColor);
-        mDarkPaint.setAlpha((int) (255 - mAlphaChange));
-        canvas.drawCircle(startPointX + mDotRadius * 2 - mMoveDistance + mDotSpacing, startPointY, mDotRadius, mDarkPaint);
+        //因为两个圆点可能会给定不同的颜色来显示 所以提供两种颜色设置mLeftColor和mRightColor
+        mPaint.setColor(mLeftColor);
+        mPaint.setAlpha((int) (255 - mAlphaChange));
+        canvas.drawCircle(startPointX + mMoveDistance, startPointY, mDotRadius, mPaint);
+        mPaint.setColor(mRightColor);
+        mPaint.setAlpha((int) (255 - mAlphaChange));
+        canvas.drawCircle(startPointX + mDotRadius * 2 - mMoveDistance + mDotSpacing, startPointY, mDotRadius, mPaint);
 
         invalidate();
     }
